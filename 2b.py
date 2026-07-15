e620004190adc421a9f4ea778ee6414cd617def4
Nothing-888 <304258016+Nothing-888@users.noreply.github.com>
2026-07-15 09:07:11 +0700

    New Update

--- 2b.py
+++ 2b.py
@@ -1,7 +1,16 @@
-name = int(input("ชื่อ : "))
+name = input("ชื่อ : ")
 age = int(input("อายุ : "))
 height = int(input("ส่วนสูง : "))
 power = int(input("พลัง : "))
 money = int(input("เงิน : "))
 
-if money >=
+if money < 100 :
+    print("ไม่รับ เพราะ จนเกินไป บอสไม่ชอบ")
+elif money >= 100 & money <= 1000 :
+    print("ไม่รับ เพราะ จน")
+elif money > 1000 & money <= 10000 :
+    print("รับ เพราะ สงสาร")
+elif money > 10000 & money <= 100000 :
+    print("รับ เพราะ พอมีเงิน")
+else :
+    print("รับ เพราะ รวย บอสชอบ")
